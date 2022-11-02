3c66d84c7dc55f8fcb81fccba5ee0dc138b35ab1
Josiel Paz <josielpaz4@gmail.com>
2022-11-02 19:11:42 -0300

    update 02-11-2022 19:11

--- src/pages/404/NotFoundPage.jsx
+++ src/pages/404/NotFoundPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Style404 = styled.div`
@@ -5,13 +6,50 @@ const Style404 = styled.div`
   width: 100vw;
   background-color: black;
   display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .div-left {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(175, 173, 173, 0.1);
+    box-shadow: 10px 10px 20px 0px rgba(255, 255, 255, 0.2);
+    border-radius: 20px;
+    color: white;
+    width: 40%;
+    height: 30%;
+    h1 {
+      font-size: 20rem;
+      margin: 1.2rem 0;
+    }
+    .link {
+      margin-top: 1rem;
+      text-decoration: none;
+      font-weight: 800;
+      color: red;
+
+      :hover {
+        color: white;
+      }
+    }
+  }
 `;
 
 export default function NotFoundPage() {
   return (
     <Style404>
-      <div className="div-letf"></div>
-      <div className="div-right"></div>
+      <div className="div-left">
+        <h1>404</h1>
+
+        <p>Pagina não encontrada</p>
+
+        <Link className="link" to="/">
+          {" "}
+          Voltar
+        </Link>
+      </div>
     </Style404>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,61 @@
+import styled from "styled-components";
+
+const StyleHome = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .div-left {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(175, 173, 173, 0.1);
+    box-shadow: 10px 10px 20px 0px rgba(255, 255, 255, 0.233);
+    border-radius: 20px;
+    padding: 1.5rem;
+    h1 {
+      color: white;
+    }
+
+    img {
+      width: 50%;
+    }
+
+    button {
+      border: 2px solid #963232;
+      border-radius: 5px;
+      width: 30%;
+      padding: 10px 0;
+      color: #963232;
+      background-color: transparent;
+      font-weight: 900;
+
+      :hover {
+        border-color: whitesmoke;
+
+        color: whitesmoke;
+        background-color: #963232;
+        font-weight: 800;
+      }
+    }
+  }
+`;
+
 export default function Home() {
+  const handleClick = () => alert("Troquei");
   return (
-    <>
-      <h1>Home</h1>
-      <p>Bem vindo a home</p>
-    </>
+    <StyleHome>
+      <div className="div-left">
+        <h1> Bem-vindo a Ferrari Industry</h1>
+
+        <img src="./assets/images/logo.png" alt="logo" />
+        <button onClick={handleClick}> Entrar</button>
+      </div>
+      <div className="div-right"></div>
+    </StyleHome>
   );
 }
